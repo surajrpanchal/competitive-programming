23b6406bf65bc77b12057154b1d52b4608cf6b06
Suraj Panchal <surajrpanchal@gmail.com>
2021-08-08 23:59:13 +0530

    initial commit

---
+++ CompetitiveProgramming/CompetitiveProgramming/Watermelon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CompetitiveProgramming
+{
+    class Watermelon
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0 && i != 2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ CompetitiveProgramming/CompetitiveProgramming/WayTooLongWords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompetitiveProgramming
+{
+    class WayTooLongWords
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            List<string> values = new List<string>();
+            for (int j = 1; j <= i; j++)
+            {
+                string value = Console.ReadLine();
+                values.Add(value.Length > 100 ? value.Remove(100) : value);
+            }
+            for (int j = 0; j < values.Count; j++)
+            {
+                string value = values[j];
+                if (value.Length > 10)
+                {
+                    string newValue2 = value.Remove(0, 1).Remove(value.Length - 2);
+                    value = string.Concat(value[0], newValue2.Length, value[^1]);
+                }
+                Console.WriteLine(value);
+            }
+        }
+
+    }
+}
